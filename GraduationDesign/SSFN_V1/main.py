5724b5fd0cd811122cccbffb431b42f14ccc2c34
sivanWu0222 <yirufeng@foxmail.com>
2019-04-21 10:50:41 +0800

    第一版：命令行下读取文件计算句子相似度

---
+++ GraduationDesign/SSFN_V1/main.py
@@ -0,0 +1,10 @@
+from SSFN_V1 import SSF_V1 as ssf
+
+if __name__ == '__main__':
+    Sen1 = input("请输入句子1：")
+    Sen2 = input("请输入句子2：")
+    ssf.init(Sen1, Sen2)
+    ssf.calc_simi_element(ssf.Sen1_word, ssf.Sen2_word, ssf.Sen1_word_vec_set, ssf.Sen2_word_vec_set)
+    ssf.init_set()
+    similarity = ssf.calc_sen_simi()
+    print("两个句子的相似度为 : ", similarity)
