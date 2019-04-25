3bacc2fafe54108e87a581bca6d3f25719601c3a
sivanWu0222 <yirufeng@foxmail.com>
2019-04-25 14:33:01 +0800

    '第4版本小改动：使用NLTK去停词和分词'

---
+++ GraduationDesign/SSFN_V4_02/SSF_V2.py
@@ -0,0 +1,166 @@
+import numpy as np
+from SSFN_V4_02 import util
+import nltk
+Sen1_word = []  # 存储句子1的各个单词
+Sen2_word = []  # 存储句子2的各个单词
+Sen1_word_vec_set = []  # 存储句子1的词向量集合
+Sen2_word_vec_set = []  # 存储句子2的词向量集合
+
+S_sen1_vec_set = []  # 存储相对于句子1的相似元对应的词向量，也就是包括在句子1中的相似元对应的词向量
+NS_sen1_vec_set = []  # 存储相对于句子1的非相似元对应的词向量
+S_sen2_vec_set = []  # 存储相对于句子2的相似元对应的词向量，也就是包括在句子2中的相似元对应的词向量
+# NS_sen2_vec_set = []  # 存储相对于句子2的非相似元对应的词向量，也就是包括在句子2中的非相似元对应的词向量
+S_sen1_word = []  # 存储相对于句子1的相似元对应的词，也就是包括在句子1中的相似元对应的词
+NS_sen1_word = []  # 存储相对于句子1的相似元对应的词，也就是包括在句子1中的相似元对应的词
+S_sen2_word = []  # 存储相对于句子1的相似元对应的词，也就是包括在句子1中的相似元对应的词
+NS_sen2_word = []  # 存储相对于句子1的相似元对应的词，也就是包括在句子1中的相似元对应的词
+u = []  # 存储相似度的集合，如果不相似写入0
+U = []  # 只存储相似度非0的集合
+# U_word = []  # 存储相似的单词
+maxsim = 0
+
+X = []
+Y = []
+
+
+def init(Sen1, Sen2):
+    """
+    初始化各个参数：包括
+                    Sen1_word
+                    Sen2_word
+                    Sen1_word_vec_set
+                    Sen2_word_vec_set
+    :param Sen1: 句子1的字符串形式
+    :param Sen2: 句子2的字符串形式
+    :return:        1 初始化成功，也就是所有单词的词向量全部找到
+                    0 初始化失败，部分单词的词向量没有找到
+    """
+    global Sen1_word
+    global Sen2_word
+    global Sen1_word_vec_set
+    global Sen2_word_vec_set
+
+    # 使用NLTK进行分词
+    Sen1_word = nltk.word_tokenize(Sen1)
+    print(Sen1_word)
+    Sen2_word = nltk.word_tokenize(Sen2)
+    print(Sen2_word)
+
+    Sen1_word_vec_set = util.get_sen_vec_set(Sen1_word)
+    if Sen1_word_vec_set == 0:
+        return 0
+    Sen2_word_vec_set = util.get_sen_vec_set(Sen2_word)
+    if Sen2_word_vec_set == 0:
+        return 0
+    return 1
+
+
+def calc_simi_element(Sa, Sb, Sa_vec, Sb_vec, u0=0.25):
+    """
+    计算相似元
+    :param Sa: 句子A字符串列表
+    :param Sb: 句子B字符串列表
+    :param Sa_vec: 句子A对应的词向量集合
+    :param Sb_vec: 句子B对应的词向量集合
+    :param u0: 相似度阈值, 默认为0.25，判断是否相似，超过阈值认为相似
+    :return: None
+    """
+    global S_sen1_vec_set
+    global NS_sen1_vec_set
+    global S_sen2_vec_set
+    # global NS_sen2_vec_set
+    global S_sen1_word
+    global NS_sen1_word
+    global S_sen2_word
+    global NS_sen2_word
+    global u
+    global U
+    global maxsim
+    for i in range(len(Sa)):
+        maxsim = 0
+
+        for j in range(len(Sb)):
+            tmp_sim = util.calc_cos_sim(Sa_vec[i], Sb_vec[j])
+            print("{0} 和 {1} 的余弦距离: {2}".format(Sa[i], Sb[j], tmp_sim))
+            if tmp_sim > maxsim:
+                maxsim = tmp_sim
+                tmp_word_b = Sb[j]
+                tmp_word_b_vec = Sb_vec[j]
+        if maxsim > u0:
+            S_sen1_vec_set.append(Sa_vec[i])
+            S_sen1_word.append(Sa[i])
+            S_sen2_vec_set.append(tmp_word_b_vec)
+            S_sen2_word.append(tmp_word_b)
+            u.append(maxsim)
+            U.append(maxsim)
+        else:
+            NS_sen1_vec_set.append(Sa_vec[i])
+            NS_sen1_word.append(Sa[i])
+            u.append(0)
+    NS_sen2_word = list(set(Sb) - set(S_sen2_word))
+    print("句子1的相似元：{0}".format(S_sen1_word))
+    print("句子2的相似元：{0}".format(S_sen2_word))
+    print("句子1的非相似元：{0}".format(NS_sen1_word))
+    print("句子2的非相似元 {0}".format(NS_sen2_word))
+
+
+def init_set():
+    """
+    初始化集合X和Y
+    :return:
+    """
+    global Sen1_word
+    global Sen2_word
+    global S_sen1_word
+    global NS_sen2_word
+    m = len(Sen1_word)
+    n = len(Sen2_word)
+    p = len(S_sen1_word)
+    q = len(NS_sen2_word)
+    N = m + n
+    for i in range(N):
+        # 初始化集合X
+        if i < m:
+            X.append(util.get_word_weight(Sen1_word[i], Sen1_word))
+        else:
+            X.append(0)
+
+        # 初始化集合Y
+        if i < p:
+            Y.append(util.get_word_weight(S_sen1_word[i], Sen1_word) * U[i])
+        elif i < (N - q):
+            Y.append(0)
+        else:
+            Y.append(util.get_word_weight(NS_sen2_word[i-N+q], Sen2_word))
+    print(S_sen1_word)
+
+
+def calc_sen_simi():
+    """
+        计算两个句子的相似性
+        :return: similarity 两个句子的相似性
+    """
+    global X
+    global Y
+    p = len(S_sen1_word)
+    m = len(Sen1_word)
+    # tmp_array = np.sum(np.array(Y[:p]))  # 相似度函数的分子
+    # 分母改为平方之后的计算
+    tmp_list = [X[i]*Y[i] for i in range(len(S_sen1_word))]
+    tmp_array = np.sum(np.array(tmp_list))
+    tmp = np.sqrt(np.sum(np.square(np.array(X[:m]))))  # 相似度函数的分母左半部分
+
+    tmp_right_left = 0.0
+    for i in range(p):
+        tmp_right_left += ((U[i] ** 2) * (util.get_word_weight(S_sen1_word[i], Sen1_word) ** 2))
+    tmp_right = np.sqrt(tmp_right_left + np.sum(np.square(np.array(Y[m+p:]))))
+    similarity = tmp_array / (tmp * tmp_right)
+    """
+    调试使用
+    # print("分子：", tmp_array)
+    # print("分母左半部分：", tmp)
+    # print("分母右半部分：", tmp_right)
+    """
+
+    return similarity
+
---
+++ GraduationDesign/SSFN_V4_02/main.py
@@ -0,0 +1,20 @@
+import sys
+from SSFN_V4_02 import view
+
+if __name__ == "__main__":
+    app = view.QApplication(sys.argv)
+    ex = view.MyFrame()
+    """
+    app.exec_()其实就是QApplication的方法，原来这个exec_()方法的作用
+    是“进入程序的主循环直到exit()被调用”，如果没有这个方法，运行的时候窗口会闪退，
+    所以show是有发挥作用的，但没有使用exec_()，所以没有进入程序的主循环就直接结束了。
+    """
+    # you are beautiful
+    # you are a person
+    sys.exit(app.exec_())
+
+    """
+    i can play some body that can body that body that body
+i can play that body that body th
+    
+    """
