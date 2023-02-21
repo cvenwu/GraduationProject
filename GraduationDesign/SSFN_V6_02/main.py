c426152aeca48eb1edbddc79988a5cafa2741436
Cven Wu <yirufeng@foxmail.com>
2023-02-21 22:53:54 +0800

    Update main.py

--- GraduationDesign/SSFN_V6_02/main.py
+++ GraduationDesign/SSFN_V6_02/main.py
@@ -2,6 +2,7 @@ import sys
 from SSFN_V6_02.view import view_data
 
 if __name__ == "__main__":
+    # 展示界面信息
     app = view_data.QApplication(sys.argv)
     ex = view_data.MyFrame()
     sys.exit(app.exec_())
